146d6cc838785cab9db9a02bf5cbd6e6fc510aac
Oladayo234 <oladayoolubunmi20@gmail.com>
2026-03-06 07:56:29 +0100

    commit

--- src/numberProfile/NumberProfile.java
+++ src/numberProfile/NumberProfile.java
@@ -22,8 +22,12 @@ public class NumberProfile {
     public static String getFactorOfNumber(int userInput) {
         String factors = "";
         for (int count = 1; count <= userInput; count++) {
-            if (userInput % count == 0)
-                factors  += count;
+            if (userInput % count == 0) {
+                if (!factors.isEmpty()) {
+                    factors += ", ";
+                }
+                factors += count;
+            }
         }
         return factors;
     }
@@ -100,7 +104,7 @@ public class NumberProfile {
         int sqrt = (int) Math.sqrt(userInput); return sqrt * sqrt == userInput;
     }
 
-    public static boolean checkNivenNumber(int userInput) {
+    public static boolean getNivenNumber(int userInput) {
         int userNumber = userInput;
         int remainder;
         int sum = 0;
@@ -112,7 +116,7 @@ public class NumberProfile {
         return userInput / sum == 0;
     }
 
-    public static boolean checkIsSpyNumber(int userInput) {
+    public static boolean getSpyNumber(int userInput) {
         int remainder;
         int sum = 0;
         int product = 1;
@@ -146,23 +150,83 @@ public class NumberProfile {
         return false;
     }
 
-        public static void getProfileOfNumber(int userInput) {
-        System.out.println("isEven:          " + (NumberProfile.checkIsEven(userInput) ? "Yes" : "No"));
-        System.out.println("isOdd:          " + (NumberProfile.checkIsOdd(userInput) ? "Yes" : "No"));
-        System.out.println("isPrime:         " + (NumberProfile.checkIsPrime(userInput) ? "Yes" : "No"));
-        System.out.println("isPerfect:       " + (NumberProfile.checkIsPerfectNumber(userInput) ? "Yes" : "No"));
-        System.out.println("isSquare:        " + (NumberProfile.checkIsSquare(userInput) ? "Yes" : "No"));
-        System.out.println("isPalindrome:    " + (NumberProfile.checkIsPalindrome(userInput) ? "Yes" : "No"));
-        System.out.println("isArmstrong:     " + (NumberProfile.checkArmstrongNumber(userInput) ? "Yes" : "No"));
-        System.out.println("perfectSquare:     " + (NumberProfile.checkIsPerfectSquare(userInput) ? "Yes" : "No"));
-        System.out.println("isNevin:     "      + (NumberProfile.checkNivenNumber(userInput) ? "Yes" : "No"));
-        System.out.println("isSpyNumber:     "      + (NumberProfile.checkIsSpyNumber(userInput) ? "Yes" : "No"));
-        System.out.println("isCube:     "      + (NumberProfile.checkIsCube(userInput) ? "Yes" : "No"));
-        System.out.println("isStrongNumber:     "      + (NumberProfile.checkIsStrongNumber(userInput) ? "Yes" : "No"));
-        System.out.println("Factors of " + userInput + ":       " + NumberProfile.getFactorOfNumber(userInput));
-        System.out.println("Square of " + userInput + ":       " + NumberProfile.getSquareOfNumber(userInput));
-        System.out.println("Factorial of " + userInput + ":       " + NumberProfile.getFactorial(userInput));
-
+    public static boolean getPositiveNumber(int userInput) {
+        return userInput > 0;
     }
+
+    public static boolean getNegativeNumber(int userInput) {
+        return userInput < 0;
+    }
+
+    public static boolean checkIfNumberIsZero(int userInput) {
+        return userInput == 0;
+    }
+
+   public static boolean getCompositeNumbers(int userInput) {
+       int divisorCount = 0;
+       for (int count = 1; count <= userInput; count++) {
+           if (userInput % count == 0) {
+               divisorCount++;
+           }
+       }
+       return divisorCount > 2;
+    }
+
+    public static boolean getAbundantNumber(int userInput) {
+        int factors = 0;
+        for (int count = 1; count < userInput; count++) {
+            if (userInput % count == 0)
+                factors  += count;
+        }
+        return factors > userInput;
+    }
+
+    public static boolean getDeficientNumber(int userInput) {
+        int factors = 0;
+        for (int count = 1; count < userInput; count++) {
+            if (userInput % count == 0)
+                factors  += count;
+        }
+        return factors < userInput;
+    }
+
+    private static int getDigitalSum(int userInput) {
+        int remainder;
+        int sum = 0;
+        while (userInput != 0) {
+            remainder = userInput % 10;
+            sum += remainder;
+            userInput /= 10;
+        }
+        return sum;
+    }
+
+
+    public static void getProfileOfNumber(int userInput) {
+        if (NumberProfile.getPositiveNumber(userInput))         System.out.println(userInput + " is a Positive Number");
+        if (NumberProfile.getNegativeNumber(userInput))         System.out.println(userInput + " is a Negative Number");
+        if (NumberProfile.checkIfNumberIsZero(userInput))         System.out.println(userInput + " is Zero");
+        if (NumberProfile.checkIsEven(userInput))         System.out.println(userInput + " is an Even Number");
+        if (NumberProfile.checkIsOdd(userInput))          System.out.println(userInput + " is an Odd Number");
+        if (NumberProfile.checkIsPrime(userInput))        System.out.println(userInput + " is a Prime Number");
+        if (NumberProfile.getCompositeNumbers(userInput))        System.out.println(userInput + " is a Composite Number");
+        if (NumberProfile.getAbundantNumber(userInput))        System.out.println(userInput + " is an Abundant Number");
+        if (NumberProfile.getDeficientNumber(userInput))        System.out.println(userInput + " is an Deficient Number");
+        if (NumberProfile.checkIsPerfectNumber(userInput))System.out.println(userInput + " is a Perfect number");
+        if (NumberProfile.checkIsSquare(userInput))       System.out.println(userInput + " is a Square number");
+        if (NumberProfile.checkIsPalindrome(userInput))   System.out.println(userInput + " is a Palindrome");
+        if (NumberProfile.checkArmstrongNumber(userInput))System.out.println(userInput + " is an Armstrong number");
+        if (NumberProfile.checkIsPerfectSquare(userInput))System.out.println(userInput + " is a Perfect Square");
+        if (NumberProfile.getNivenNumber(userInput))    System.out.println(userInput + " is a Niven number");
+        if (NumberProfile.getSpyNumber(userInput))    System.out.println(userInput + " is a Spy number");
+        if (NumberProfile.checkIsCube(userInput))         System.out.println(userInput + " is a Cube number");
+        if (NumberProfile.checkIsStrongNumber(userInput)) System.out.println(userInput + " is a Strong number");
+
+        System.out.println("Factors:   " + NumberProfile.getFactorOfNumber(userInput));
+        System.out.println("Square:    " + NumberProfile.getSquareOfNumber(userInput));
+        System.out.println("Factorial: " + NumberProfile.getFactorial(userInput));
+        System.out.println("Digital Sum: " + NumberProfile.getDigitalSum(userInput));
+    }
+
 
 }
